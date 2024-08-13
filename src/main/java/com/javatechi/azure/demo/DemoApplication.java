d161e7d58c531f58bfaea2be87e8c9298f03a86a
SariaEssid <saria.essid@esprit.tn>
2024-08-13 02:24:05 +0100

    first commit

--- src/main/java/com/javatechi/azure/demo/DemoApplication.java
+++ src/main/java/com/javatechi/azure/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.javatechi.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
